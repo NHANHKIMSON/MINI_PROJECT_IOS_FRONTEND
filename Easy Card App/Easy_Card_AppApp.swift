d4d3a91b6da04677cc4893ab07055a0654359df2
nhanhkimson.biu@gmail.com <nhanhkimson.biu@gmail.com>
2025-09-19 21:42:50 +0700

    Initial Commit

---
+++ Easy Card App/Easy_Card_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Easy_Card_AppApp.swift
+//  Easy Card App
+//
+//  Created by Apple on 9/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Easy_Card_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Easy Card AppTests/Easy_Card_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Easy_Card_AppTests.swift
+//  Easy Card AppTests
+//
+//  Created by Apple on 9/19/25.
+//
+
+import Testing
+@testable import Easy_Card_App
+
+struct Easy_Card_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
