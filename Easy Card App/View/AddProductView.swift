f833bbd4945b44b86d198dd8db6998fceb1a3866
ymengsea <mrmengsea@gmail.com>
2025-09-26 20:11:24 +0700

    Completed static UI but not yet validate.

--- Easy Card App/View/AddProductView.swift
+++ Easy Card App/View/AddProductView.swift
@@ -1,38 +1,246 @@
 import SwiftUI
+import PhotosUI
 
 struct AddProductView: View {
     @Environment(\.dismiss) var dismiss
+    
+    @State private var category: String = ""
+    @State private var title = ""
+    @State private var price = ""
+    @State private var condition = ""
+    @State private var brand = ""
+    @State private var model = ""
+    @State private var color = ""
+    @State private var year = ""
+    
+    @State private var isSelect = false
+    @State private var isExpand = false
+    
+    @State private var selectedItems: [PhotosPickerItem] = []
+    @State private var selectedImages: [UIImage] = []
+    
+
     var body: some View {
         NavigationStack {
-            VStack {
-                Text("Hello world")
-                VStack{
-                    Text("Photos")
-                    Image(systemName: "plus")
-                        .foregroundStyle(.white)
-                        .padding(9)
-                        .background(Color(.primary))
-                        .cornerRadius(50)
-
-                }
-            }
-            .padding()
-            .toolbar {
-                ToolbarItem(placement: .navigation) {
-                    Button {
-                        dismiss()
-                    } label: {
-                        Image(systemName: "xmark")
+            ZStack {
+                Color(.whiteBackground)
+                    .ignoresSafeArea()
+                VStack {
+                    VStack {
+                        Text("Photos")
                             .bold()
-                            .padding(8)
-                            .foregroundStyle(.black)
-                            .background(Color(.black).opacity(0.3))
-                            .cornerRadius(50)
+                            .font(.title3)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        if !selectedImages.isEmpty {
+                            HStack{
+                                ScrollView(.horizontal){
+                                    HStack(spacing: 20){
+                                        ForEach(selectedImages.indices, id: \.self) { index in
+                                            let img = selectedImages[index]
+                                            Image(uiImage: img)
+                                                .resizable()
+                                                .scaledToFill()
+                                                .frame(width: 100, height: 100)
+                                                .cornerRadius(10)
+                                                .clipped()
+                                                .overlay(alignment: .topTrailing) {
+                                                    Button{
+                                                        selectedImages.remove(at: index)
+                                                    }label:{
+                                                        Image(systemName: "xmark")
+                                                            .padding(5)
+                                                            .bold()
+                                                            .foregroundStyle(Color.white)
+                                                            .background(Color(.gray).opacity(0.8))
+                                                            .cornerRadius(50)
+                                                            .padding(5)
+                                                    }
+                                                }
+                                        }
+                                    }
+                                }.scrollIndicators(.hidden, axes: .horizontal)
+                                PhotosPicker(selection: $selectedItems, matching: .images){
+                                    HStack{
+                                        Image(systemName: "plus")
+                                            .font(.title3)
+                                            .padding(10)
+                                            .foregroundStyle(.white)
+                                            .background(Color(.primary))
+                                            .cornerRadius(50)
+                                    }
+                                    .frame(width: 100, height: 100)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .stroke(
+                                                style: StrokeStyle(lineWidth: 1, dash: [7])
+                                            )
+                                            .foregroundColor(.gray)
+                                    )
+                                    .onChange(of: selectedItems){ newValue in
+                                        Task{
+                                            selectedImages.removeAll()
+                                            for item in newValue {
+                                                if let data = try? await item.loadTransferable(type: Data.self){
+                                                    if let uiImage = UIImage(data: data){
+                                                        selectedImages.append(uiImage)
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }else{
+                            HStack {
+                                PhotosPicker(selection: $selectedItems, matching: .images){
+                                    Image(systemName: "plus")
+                                        .font(.title3)
+                                        .padding(10)
+                                        .foregroundStyle(.white)
+                                        .background(Color(.primary))
+                                        .cornerRadius(50)
+                                }
+                            }
+                            .padding(20)
+                            .frame(maxWidth: .infinity, maxHeight: 100)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(
+                                        style: StrokeStyle(lineWidth: 1, dash: [7])
+                                    )
+                                    .foregroundColor(.gray)
+                            )
+                            .onChange(of: selectedItems){ newValue in
+                                Task{
+                                    selectedImages.removeAll()
+                                    for item in newValue {
+                                        if let data = try? await item.loadTransferable(type: Data.self){
+                                            if let uiImage = UIImage(data: data){
+                                                selectedImages.append(uiImage)
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(.white)
+                    .cornerRadius(20)
+                    .padding(.horizontal)
+
+                    List {
+                        Button {
+                            isSelect = true
+                        } label: {
+                            HStack {
+                                Text("Categories")
+                                    .foregroundStyle(.black)
+                                Spacer()
+                                Text(category == "" ? "Select one" : category )
+                                    .foregroundStyle( category == "" ? Color(.primary) : .black)
+                                Image(systemName: "chevron.right")
+                                    .foregroundStyle(.gray)
+                            }
+                        }
+                        .navigationDestination(isPresented: $isSelect) {
+                            // select categories
+                            SelectCategoriesView(category: $category )
+                                .navigationBarBackButtonHidden()
+                        }
+                        HStack {
+                            Text("Title")
+                                .frame(width: 100, alignment: .leading)
+                            TextField("Input", text: $title)
+                        }
+                        HStack {
+                            Text("Price")
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            TextField("$0.00", text: $price)
+                                .frame(
+                                    maxWidth: .infinity,
+                                    alignment: .trailing
+                                )
+                                .multilineTextAlignment(.trailing)
+                        }
+                        Button {
+                            isExpand.toggle()
+                        } label: {
+                            HStack {
+                                Text("More Details")
+                                    .foregroundStyle(.black)
+                                Spacer()
+                                if isExpand {
+                                    Image(systemName: "chevron.right")
+                                        .foregroundStyle(.gray)
+                                        .rotationEffect(.degrees(90))
+                                }else {
+                                    Image(systemName: "chevron.right")
+                                        .foregroundStyle(.gray)
+                                        .rotationEffect(.zero)
+                                }
+                            }
+                        }
+                        if isExpand {
+                            HStack {
+                                Text("Condition")
+                                    .frame(width: 100, alignment: .leading)
+                                TextField("Used, New...", text: $condition)
+                            }
+                            HStack {
+                                Text("Brand")
+                                    .frame(width: 100, alignment: .leading)
+                                TextField("Input", text: $brand)
+                            }
+                            HStack {
+                                Text("Model")
+                                    .frame(width: 100, alignment: .leading)
+                                TextField("Input", text: $model)
+                            }
+                            HStack {
+                                Text("Color")
+                                    .frame(width: 100, alignment: .leading)
+                                TextField("White, Black...", text: $color)
+                            }
+                            HStack {
+                                Text("Year")
+                                    .frame(width: 100, alignment: .leading)
+                                TextField("Input", text: $year)
+                            }
+                        }
+                    }
+                    .cornerRadius(20)
+                    
+                    Button{
+                        //
+                    }label: {
+                        Text("Post")
+                            .frame(maxWidth: .infinity)
+                            .padding(.vertical, 10)
+                            .foregroundStyle(.white)
+                            .background(Color(.primary))
+                            .cornerRadius(20)
+                            .padding(.horizontal)
                     }
                 }
-                ToolbarItem(placement: .principal) {
-                    Text("Add item")
-                        .bold()
+                .toolbar {
+                    ToolbarItem(placement: .navigation) {
+                        Button {
+                            dismiss()
+                        } label: {
+                            Image(systemName: "xmark")
+                                .bold()
+                                .padding(8)
+                                .foregroundStyle(.black)
+                                .background(Color(.black).opacity(0.3))
+                                .cornerRadius(50)
+                        }
+                    }
+                    ToolbarItem(placement: .principal) {
+                        Text("Add item")
+                            .bold()
+                    }
                 }
             }
         }
---
+++ Easy Card App/View/SelectCategoriesView.swift
@@ -0,0 +1,58 @@
+import SwiftUI
+
+struct SelectCategoriesView: View {
+    var categories = [
+        (name: "Food & Drink", icon: "takeoutbag.and.cup.and.straw"),
+        (name: "Vehicle", icon: "car"),
+        (name: "Electronic Devices", icon: "tv.and.mediabox.fill"),
+        (name: "Properties", icon: "house.and.flag"),
+        (name: "Furniture", icon: "fan.desk"),
+        (name: "Pet", icon: "pawprint"),
+        (name: "Hobbies", icon: "guitars")
+    ]
+    @Environment(\.dismiss) var dismiss
+    @Binding var category: String
+    
+    var body: some View {
+        List{
+            ForEach(categories.indices, id: \.self) { index in
+                let item = categories[index]
+                Button{
+                    category = item.name
+                    dismiss()
+                } label:{
+                    HStack{
+                        Image(systemName: item.icon)
+                            .frame(width: 30)
+                            .padding(.trailing)
+                        Text(item.name)
+                    }
+                    .font(.title3)
+                    .foregroundStyle(Color.black)
+                    .padding(10)
+                }
+            }
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigation) {
+                Button{
+                    dismiss()
+                }label: {
+                    Image(systemName: "arrow.backward")
+                        .padding(8)
+                        .frame(width: 40, height: 40)
+                        .font(.title3)
+                        .bold()
+                        .foregroundStyle(.black)
+                        .background(Color.gray.opacity(0.8))
+                        .cornerRadius(50)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+//    SelectCategoriesView(category: .constant("") )
+    AddProductView()
+}
